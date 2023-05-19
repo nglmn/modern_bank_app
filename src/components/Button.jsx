d2576b42b79d19152561d2c26abea25741925798
artem <artem140742@gmail.com>
2023-05-19 23:32:33 +0300

    add business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
     return (
-        <div>Button</div>
+        <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-black outline-none rounded-xl ${styles}`} >
+            Get Started
+        </button >
     )
 }
 
