894ddfbe9d3b567db3cf19230bef28ef291b2386
artem <artem140742@gmail.com>
2023-05-23 21:12:34 +0300

    add Clients components

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,8 +6,9 @@ const Billing = () => (
         <div className={layout.sectionReverse}>
             <img src={bill} alt="billing" className="w-[55%] h-[100%] relative z-[5]" />
 
-            <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+            <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
             <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+            <div className="absolute z-[0] -left-1/2 w-[50%] h-[50%] -left-[50%] rounded-full white__gradient" />
 
             <div className={`${layout.sectionInfo} ml-15`}>
                 <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing.</h2>
